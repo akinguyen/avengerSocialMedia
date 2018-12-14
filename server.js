cbe7353577cc77e92c04f3c55154e406ed77e23d
thaominh123 <duynguyenquykhanh@gmail.com>
2018-12-14 12:42:05 -0800

    Updated

--- server.js
+++ server.js
@@ -52,5 +52,5 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.listen(port, () => console.log("Running " + port));
