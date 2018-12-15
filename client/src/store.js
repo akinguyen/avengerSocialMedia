e091b331b50f3d9468ba881254a116717759bcc6
thaominh123 <duynguyenquykhanh@gmail.com>
2018-12-14 18:39:06 -0800

    Updated

--- client/src/store.js
+++ client/src/store.js
@@ -2,11 +2,16 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
+let devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "prod") {
+  devTools = a => a;
+}
 export default createStore(
   rootReducer,
   {},
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    devTools
   )
 );
